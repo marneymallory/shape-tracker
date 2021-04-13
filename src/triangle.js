c3515515f0647a0e2c4bd8a48354f34e064a5343
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-04-12 16:31:56 -0800

    passed third test

--- src/triangle.js
+++ src/triangle.js
@@ -7,4 +7,11 @@ Triangle.prototype.checkType = function() {
   Triangle.prototype.checkType = function() {
     return "not a triangle";  
   };
+  Triangle.prototype.checkType = function() {
+    if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+      return "not a triangle";
+    } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+      return "scalene triangle";
+    }
+  };
 };
